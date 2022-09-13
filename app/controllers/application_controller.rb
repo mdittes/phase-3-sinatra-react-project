e60a9c85e38bd6e92759866b877dd54015f503bf
Matt Dittes <mattdittes@gmail.com>
2022-09-13 17:08:44 -0400

    Initial dummy routes created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,29 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
- 
+  get "/states" do
+    states = State.all
+    states.to_json
+  end
+
+  get "/temperatures" do
+    temps = Temperature.all
+    temps.to_json
+  end
+
+  get "/living_costs" do 
+    living_costs = LivingCost.all
+    living_costs.to_json
+  end
+
+  get "/legal_weed" do 
+    legal_weed = MarijuanaLaw.all
+    legal_weed.to_json
+  end
+
+  get "/life_expectancy" do 
+    life_expectancy = LifeExpectancy.all
+    life_expectancy.to_json
+  end
 
 end
